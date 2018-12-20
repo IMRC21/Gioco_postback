3abe08da5b0797c1e265314cb85758ff076d535a
imrc <marianoradu@mail.com>
2018-12-20 13:26:44 +0100

    Asd

--- index.php
+++ index.php
@@ -6,23 +6,29 @@
     <title>Gioco</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src='main.js'></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"
+            integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+            crossorigin="anonymous"></script>
+    
     
 </head>
 <body>
     <?php
         if($_POST){
-            echo "";
-            echo "<center><div id='output'></div>
-            
-            </center>";
-
+            echo "Il messaggio è: " . "<div id='cripto'>" . $_POST["nome"] . "</div>";
+            echo "<div id='output'>
+            </div>
+            ";
+            echo "<button id='indietro' onclick='window.history.back()'> Torna indietro </button>";
+            echo "<input type='hidden' id='indietro'>";
+            echo "<script src='main.js'></script>";
         }else{
             echo "<h1> Bevenuto </h1>";
             echo "
             <form action ='index.php' method='POST' >
             <label> Inserisci la frase da criptare </label>
-            <input type='text' name='nome' id='Nome'>
+            <input type='text' name='nome' >
             <input type='submit'>
             ";
         }
--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 
 var nome = document.getElementById("nom");
 var opt = document.getElementById("output");
+var alfa = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var someDivs = document.createElement("div");
-console.log(someDivs);
-someDivs.setAttribute("class","mini");
-for(var i=0;i<24;i++){
-    opt.appendChild(someDivs);
+var color = ["#16A085","#F4D03F","#CA6F1E","#CB4335","#B3B6B7","#F4D05F","#16A085","#F4D03F","#CA6F1E","#CB4335","#B3B6B7","#F4D05F","#16A085","#F4D03F","#CA6F1E","#CB4335","#B3B6B7","#F4D05F","#16A085","#F4D03F","#CA6F1E","#CB4335","#B3B6B7","#F4D05F","#16A085","#F4D03F","#CA6F1E","#CB4335","#B3B6B7","#F4D05F","#16A085","#F4D03F","#CA6F1E","#CB4335","#B3B6B7","#F4D05F"]
+var ilMessaggio = document.getElementById("cripto").innerHTML;
+ilMessaggioSplit = ilMessaggio.split("");
+console.log(ilMessaggioSplit);
+var pos;
+for(var i=0;i<ilMessaggioSplit.length;i++){
+    for(var ct = 0; ct<36;ct++){
+        if(ilMessaggioSplit[i] == alfa[ct]){
+            pos = ct;
+            document.getElementById("output").innerHTML += "<div class='mini' onclick='rimuovi(this.id," + i + " )' id='" + alfa[pos] + "' ></div>";
+            document.getElementById(""+alfa[pos]).setAttribute("style", "background-color:"+ color[pos]);
+        }
+        
+    }
+}
+
+function rimuovi(id,p){
+    console.log(id);
+    console.log(p);
 }
